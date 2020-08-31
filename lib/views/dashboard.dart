4c1429e3954ab3ab79499ff3b172f4ed64ccec1d
Marius Preikschat <m.preikschat@gmail.com>
2020-08-31 18:28:34 +0200

    #42

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -49,14 +49,10 @@ class _DashboardWidgetState extends State<DashboardWidget> {
   }
 
   Widget daysUntil() {
-    var now = new DateTime.now();
-    var year = now.year.toString();
-    var month = now.month.toString().padLeft(2, '0');
-    var day = now.day.toString();
-    var today = DateTime.parse('$year-$month-$day');
-
+    var now = new DateTime.now().toUtc();
     var nextRacedate = DateTime.parse(nextRaceDate);
-    int difference = today == nextRacedate ? 0 : (nextRacedate.difference(today).inDays);
+    int differenceDays = nextRacedate.difference(now).inDays;
+    int differenceHours = nextRacedate.difference(now).inHours - differenceDays*24;
 
     return RichText(
       text: TextSpan(
@@ -64,11 +60,18 @@ class _DashboardWidgetState extends State<DashboardWidget> {
         style: TextStyle(
           color: Colors.white
         ),
-        children: difference > 0 ? <TextSpan>[
+        // children: difference > 0 ? <TextSpan>[
+        //   TextSpan(text: 'in '),
+        //   TextSpan(text: '$difference', style: TextStyle(fontWeight: FontWeight.bold)),
+        //   TextSpan(text: ' Tagen'),
+        // ] : <TextSpan>[TextSpan(text: 'Heute')],
+        children: <TextSpan>[
           TextSpan(text: 'in '),
-          TextSpan(text: '$difference', style: TextStyle(fontWeight: FontWeight.bold)),
-          TextSpan(text: ' Tagen'),
-        ] : <TextSpan>[TextSpan(text: 'Heute')],
+          TextSpan(text: '$differenceDays', style: TextStyle(fontWeight: FontWeight.bold)),
+          TextSpan(text: ' Tagen & '),
+          TextSpan(text: '$differenceHours', style: TextStyle(fontWeight: FontWeight.bold)),
+          TextSpan(text: ' Stunden'),
+        ],
       )
     );
   }
@@ -76,30 +79,38 @@ class _DashboardWidgetState extends State<DashboardWidget> {
   void getNextRaceName() async {
     final response = await http.get('http://ergast.com/api/f1/current/next.json');
     String nextRacename;
+    String nextRacecity;
 
     if (response.statusCode == 200) {
       nextRacename = json.decode(response.body)['MRData']['RaceTable']['Races'][0]['raceName'];
+      nextRacecity = json.decode(response.body)['MRData']['RaceTable']['Races'][0]['Circuit']['Location']['locality'];
     } else {
       nextRacename = json.decode('{ "MRData": { "RaceTable": { "Race": ["raceName": "next Race"] } } }')['MRData']['RaceTable']['Races'][0]['raceName'];
+      nextRacecity = "Unknown";
     }
 
     setState(() {
-      nextRaceName = '$nextRacename';
+      nextRaceName = '$nextRacename at $nextRacecity';
     });
   }
 
   void getNextRaceDate() async {
     final response = await http.get('http://ergast.com/api/f1/current/next.json');
     String nextRacedate;
+    String nextRacetime;
 
     if (response.statusCode == 200) {
       nextRacedate = json.decode(response.body)['MRData']['RaceTable']['Races'][0]['date'];
+      nextRacetime = json.decode(response.body)['MRData']['RaceTable']['Races'][0]['time'];
     } else {
       String year = new DateTime.now().year.toString();
       String month = new DateTime.now().month.toString();
       String day = new DateTime.now().day.toString();
       nextRacedate = json.decode('{ "MRData": { "RaceTable": { "Race": ["date": "$year-$month-$day"] } } }')['MRData']['RaceTable']['Races'][0]['date'];
+      nextRacetime = "00:00:00Z";
     }
+
+    nextRacedate = nextRacedate + ' ' + nextRacetime;
 
     setState(() {
       nextRaceDate = '$nextRacedate';
